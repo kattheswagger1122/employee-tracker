7f8312978bdd849f4137131eb2e62e2976599fd8
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:51:50 -0400

    functions for you to view the company's budget

--- server.js
+++ server.js
@@ -500,7 +500,7 @@ const addNewDepartment = () => {
     });
   };
 
-  
+
   const deleteEmployee = () => {
     connection.query("SELECT * FROM EMPLOYEE", (err, res) => {
       if (err) throw err;
@@ -537,3 +537,46 @@ const addNewDepartment = () => {
     });
   };
   
+
+  const viewBudget = () => {
+    connection.query("SELECT * FROM DEPARTMENT", (err, res) => {
+      if (err) throw err;
+  
+      const depChoice = [];
+      res.forEach(({ name, id }) => {
+        depChoice.push({
+          name: name,
+          value: id
+        });
+      });
+  
+      let questions = [
+        {
+          type: "list",
+          name: "id",
+          choices: depChoice,
+          message: "which department's budget do you want to see?"
+        }
+      ];
+  
+      inquier.prompt(questions)
+      .then(response => {
+        const query = `SELECT D.name, SUM(salary) AS budget FROM
+        EMPLOYEE AS E LEFT JOIN ROLE AS R
+        ON E.role_id = R.id
+        LEFT JOIN DEPARTMENT AS D
+        ON R.department_id = D.id
+        WHERE D.id = ?
+        `;
+        connection.query(query, [response.id], (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          startPrompt();
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    });
+  
+  };
