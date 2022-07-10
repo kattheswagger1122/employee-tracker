470877ad22591bfb2c4e587bb643acec5a341a14
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:44:56 -0400

    functions that allows you to view employee by the name of the manager

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ const addNewDepartment = () => {
       })
     });
   }
-  
+
   const updateRole = () => {
     //get all the employee list 
     connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
@@ -313,3 +313,57 @@ const addNewDepartment = () => {
     });
   }
   
+  const viewEmployeeByManager =  () => {
+    //get all the employee list 
+    connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
+      if (err) throw err;
+      const employeeChoice = [{
+        name: 'None',
+        value: 0
+      }];
+      emplRes.forEach(({ first_name, last_name, id }) => {
+        employeeChoice.push({
+          name: first_name + " " + last_name,
+          value: id
+        });
+      });
+       
+      let questions = [
+        {
+          type: "list",
+          name: "manager_id",
+          choices: employeeChoice,
+           message: "whose role do you want to update?"
+        },
+      ]
+    
+      inquier.prompt(questions)
+        .then(response => {
+          let manager_id, query;
+          if (response.manager_id) {
+            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+            WHERE E.manager_id = ?;`;
+          } else {
+            manager_id = null;
+            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+            WHERE E.manager_id is null;`;
+          }
+          connection.query(query, [response.manager_id], (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            startPrompt();
+          });
+        })
+        .catch(err => {
+          console.error(err);
+        }); 
+    });
+  }
