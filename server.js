fd2db57e268bde2bd5420df5457b7406f8c22e4c
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:49:04 -0400

    functions to delete roles

--- server.js
+++ server.js
@@ -463,3 +463,39 @@ const addNewDepartment = () => {
       });
     });
   };
+  const deleteRole = () => {
+    const departments = [];
+    connection.query("SELECT * FROM ROLE", (err, res) => {
+      if (err) throw err;
+  
+      const roleChoice = [];
+      res.forEach(({ title, id }) => {
+        roleChoice.push({
+          name: title,
+          value: id
+        });
+      });
+  
+      let questions = [
+        {
+          type: "list",
+          name: "id",
+          choices: roleChoice,
+          message: "which role do u want to delete?"
+        }
+      ];
+  
+      inquier.prompt(questions)
+      .then(response => {
+        const query = `DELETE FROM ROLE WHERE id = ?`;
+        connection.query(query, [response.id], (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} row(s) successfully deleted!`);
+          startPrompt();
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    });
+  };
