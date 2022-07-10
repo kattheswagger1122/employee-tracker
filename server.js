5bf497933fab5bb2467fc5cd565da3fa4d34772d
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:33:17 -0400

    prompt sections define the question types and functions

--- server.js
+++ server.js
@@ -25,3 +25,65 @@ connection.connect((err) => {
     startPrompt();
   });
 });
+
+function startPrompt() {
+    const startQuestion = [{
+      type: "list",
+      name: "action",
+      message: "what would you like to do?",
+      loop: false,
+      choices: ["View all employees", "View all roles", "View all departments", "add an employee", "add a role", "add a department", "update role for an employee", "update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
+    }]
+    
+    inquier.prompt(startQuestion)
+    .then(response => {
+      switch (response.action) {
+        case "View all employees":
+          viewAll("EMPLOYEE");
+          break;
+        case "View all roles":
+          viewAll("ROLE");
+          break;
+        case "View all departments":
+          viewAll("DEPARTMENT");
+          break;
+        case "add a department":
+          addNewDepartment();
+          break;
+        case "add a role":
+          addNewRole();
+          break;
+        case "add an employee":
+          addNewEmployee();
+          break;
+        case "update role for an employee":
+          updateRole();
+          break;
+        case "view employees by manager":
+          viewEmployeeByManager();
+          break;
+        case "update employee's manager":
+          updateManager();
+          break;
+        case "delete a department":
+          deleteDepartment();
+          break;
+        case "delete a role":
+          deleteRole();
+          break;
+        case "delete an employee":
+          deleteEmployee();
+          break;
+        case "View the total utilized budget of a department":
+          viewBudget();
+          break;
+        default:
+          connection.end();
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+  
+    
