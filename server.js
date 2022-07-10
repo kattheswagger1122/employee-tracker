e490262520851e8015ea896591e8a4700e0951f8
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:38:05 -0400

    new department section

--- server.js
+++ server.js
@@ -112,4 +112,25 @@ function startPrompt() {
     });
   };
 
+const addNewDepartment = () => {
+    let questions = [
+      {
+        type: "input",
+        name: "name",
+        message: "what is the new department name?"
+      }
+    ];
   
+    inquier.prompt(questions)
+    .then(response => {
+      const query = `INSERT INTO department (name) VALUES (?)`;
+      connection.query(query, [response.name], (err, res) => {
+        if (err) throw err;
+        console.log(`Successfully inserted ${response.name} department at id ${res.insertId}`);
+        startPrompt();
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
