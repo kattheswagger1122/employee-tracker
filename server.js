8e0ba9de060e248c12d9db5c432a0d068aea3e85
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-10 16:31:27 -0400

    define the syntax and connect server.js to db

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const mysql = require('mysql');
+const inquier = require('inquirer');
+const cTable = require('console.table');
+const figlet = require('figlet');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'employee_DB',
+});
+
+// Connect to the DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  figlet('Employee tracker', function(err, data) {
+    if (err) {
+      console.log('ascii art not loaded');
+    } else {
+      console.log(data);
+    }  
+    startPrompt();
+  });
+});
